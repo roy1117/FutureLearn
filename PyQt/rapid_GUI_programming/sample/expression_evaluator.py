ca2abfd6c04689ca13b1c30ec7d3f1b0cd3428af
taehyun <xogus1372@gmail.com>
2022-01-16 22:47:35 +0900

    added pop_up_alert.py

--- PyQt/rapid_GUI_programming/sample/expression_evaluator.py
+++ PyQt/rapid_GUI_programming/sample/expression_evaluator.py
@@ -1,3 +1,35 @@
+from __future__ import division
+import sys
 from math import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
-print(sqrt(4))
+class Form(QDialog):
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        self.browser = QTextBrowser()
+        self.lineedit = QLineEdit("Type an expression and press Enter")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.browser)
+        layout.addWidget(self.lineedit)
+        self.setLayout(layout)
+        self.lineedit.selectAll()
+        self.lineedit.setFocus()
+        self.lineedit.returnPressed.connect(self.updateUi)
+        # self.connect(self.lineedit, SIGNAL("returnPressed()"), self.updateUi)
+        self.setWindowTitle("Calculate")
+
+    def updateUi(self):
+        try:
+            text = self.lineedit.text()
+            self.browser.append("%s = <b>%s</b>" % (text, eval(text)))
+        except:
+            self.browser.append("<font color=red>%s is invalid!</font>" % text)
+
+app = QApplication(sys.argv)
+app.setQuitOnLastWindowClosed(False)
+form = Form()
+form.show()
+app.exec_()
