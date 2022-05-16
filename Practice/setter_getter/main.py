ff490a76b66ca480d4fcca6b82f854abe0856c2a
roy1117 <85148810+roy1117@users.noreply.github.com>
2022-05-16 16:37:34 +0900

    editted setter and getter

--- Practice/setter_getter/main.py
+++ Practice/setter_getter/main.py
@@ -17,7 +17,31 @@ class Student:
     message = 'This is help message'
     name = property(fset=set_name, fget=get_name, fdel=del_name, doc=message)
 
+class Student2:
+    def __init__(self, name):
+        self._name = name
+
+    @property
+    def name(self):
+        print('getter method is called')
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        print('setter method is called')
+        self._name = name
+
+    @name.deleter
+    def name(self):
+        print('delete method is called')
+        del self._name
+
 roy = Student('Roy')
 roy.name = 'Roy!'
 print(roy.name)
 del(roy.name)
+print('---------------------------')
+jake = Student2('Jake')
+jake.name = 'Jake!'
+print(jake.name)
+del(jake.name)
