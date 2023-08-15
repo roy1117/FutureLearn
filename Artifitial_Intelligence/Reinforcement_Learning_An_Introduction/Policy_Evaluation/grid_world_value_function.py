877226b9592d4d58834ce3d082957f6f91197645
DotNetTutorial <85148810+roy1117@users.noreply.github.com>
2023-08-16 00:08:14 +0900

    added files

--- Artifitial_Intelligence/Reinforcement_Learning_An_Introduction/Policy_Evaluation/grid_world_value_function.py
+++ Artifitial_Intelligence/Reinforcement_Learning_An_Introduction/Policy_Evaluation/grid_world_value_function.py
@@ -1,16 +1,20 @@
 import numpy as np
-
+# Declaring two arrays to store both old and new state values
 VS1 = np.zeros((4, 4))
 VS2 = np.zeros((4, 4))
 
-
+# The function takes old state values and gives new state values
 def update_value_function(old_value_function):
+    # To store new state values temporarily
     new_value_function = np.zeros((4, 4))
     for i, j in np.ndindex(old_value_function.shape):
+        # To skip update in case of terminal states
         if (i == 0 and j == 0) or (i == 3 and j == 3):
             pass
         else:
+            # We are going to accumulate all possible immediate reward and next state value pairs
             summation = 0
+            # Four actions are possible
             for k in range(4):
                 if k == 0:
                     # upward
@@ -28,6 +32,7 @@ def update_value_function(old_value_function):
                     # right
                     x = i
                     y = j + 1
+                # Make position unchanged in case we take off the grid action
                 if x < 0:
                     x = 0
                 if x > 3:
@@ -41,7 +46,10 @@ def update_value_function(old_value_function):
     return new_value_function
 
 if __name__ == '__main__':
-    for i in range(2000):
+    # k is how many times we are going to calculate approximation
+    k = 2000
+    for i in range(k):
         VS2 = update_value_function(VS1)
         VS1 = VS2
+    print('K = {0}, Our approximation is'.format(k))
     print(VS2)
